3ba0a25d60ac76b47c3e6e856b5eae1b878e124c
Sumaiya Usman <sumaiyausman@Sumaiyas-MacBook-Pro.local>
2024-07-26 17:37:04 -0400

    Fix unescaped single quote in HeroSection.jsx

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,4 +1,3 @@
-
 "use client";
 import React from "react";
 import Image from "next/image";
@@ -11,7 +10,7 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-                Hello, I'm {""}
+                Hello, I&apos;m {""}
             </span>
             <br></br>
             <TypeAnimation
@@ -37,12 +36,19 @@ const HeroSection = () => {
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
                 Hire Me
             </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
-                <span className="block bg-[#121212] over:bg-slate-800 rounded-full px-5 py-2">Resume</span>
-            </button>
+            <a 
+              href="/sumaiya_resume.pdf" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="block w-full sm:w-fit mt-3"
+            >
+              <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white">
+                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Resume</span>
+              </button>
+            </a>
           </div>
         </div>
-        <div className="col-span-5 place-self-center mt-4 lg:t-0">
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] flex justify-center items-center relative">
             <Image 
               src="/images/memoji.png" 
