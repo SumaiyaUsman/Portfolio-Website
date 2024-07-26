be269fa868d69a17ec1f5439003fc95230d4a630
Sumaiya Usman <sumaiyausman@Sumaiyas-MacBook-Pro.local>
2024-07-26 18:24:15 -0400

    move resume to public directory

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
                 Hire Me
             </button>
             <a 
-              href="/sumaiya_resume.pdf" 
+              href="/public/sumaiya_resume.pdf" 
               target="_blank" 
               rel="noopener noreferrer"
               className="block w-full sm:w-fit mt-3"
